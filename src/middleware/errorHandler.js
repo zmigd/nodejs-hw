016a95a567b6fd324d81e206abae771704cda0f4
zmigd <zmigdalovich@gmail.com>
2025-11-10 11:02:41 +0200

    edit error

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,7 +1,15 @@
+import { isHttpError } from 'http-errors';
+
 export const errorHandler = (err, req, res, next) => {
   console.error(err);
 
   const isProd = process.env.NODE_ENV === "production";
+
+  if (isHttpError(err)) {
+    return res.status(err.status).json({
+      message: err.message,
+    });
+  }
 
   res.status(500).json({
     message: isProd
